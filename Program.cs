cb173a280e13183e5f6d576afaac7f705c2ded73
Nayhee <nathantraczewski@gmail.com>
2022-05-23 13:09:32 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 42;
+            
+            Console.WriteLine("Guess the secret number!");
+            string userGuessString = Console.ReadLine();
+            int userGuess = int.Parse(userGuessString);
+
+            if(secretNumber == userGuess) 
+            {
+                Console.WriteLine("Wow! That's Correct! Great Guess!");
+            }
+            else 
+            {
+                Console.WriteLine("Nope! Try again!");
+            }
+        }
+    }
+}
